fb0c40ef9bf31c56b92532c249b6a18641a52b5e
Onyshko <onisko417@gmail.com>
2024-08-09 16:06:22 +0300

    Added connections to db

--- RegApi.Repository/obj/Debug/net8.0/RegApi.Repository.AssemblyInfo.cs
+++ RegApi.Repository/obj/Debug/net8.0/RegApi.Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RegApi.Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca55e74bb107a4558f7a0de200139c59dc00b796")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01f7e9621b2fdc6d54c5413e71995eb5493a3e03")]
 [assembly: System.Reflection.AssemblyProductAttribute("RegApi.Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RegApi.Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
