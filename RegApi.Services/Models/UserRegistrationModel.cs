bf1a1a680ecbce1bd2251c362fc1a2cd84a245d7
Onyshko <onisko417@gmail.com>
2024-08-13 14:27:39 +0300

    Some corrections

--- RegApi.Services/Models/UserRegistrationModel.cs
+++ RegApi.Services/Models/UserRegistrationModel.cs
@@ -7,10 +7,13 @@ namespace RegApi.Services.Models
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
 
-        [Required(ErrorMessage = "Email is required.")]
+        [Required(ErrorMessage = "Email is required")]
         public string? Email { get; set; }
 
-        [Required(ErrorMessage = "Password is required.")]
+        [Required(ErrorMessage = "Password is required")]
         public string? Password { get; set; }
+
+        [Required(ErrorMessage = "The password and confirmation password do not match")]
+        public string? ConfirmPassword { get; set; }
     }
 }
