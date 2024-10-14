f88f1b7cabdca8523045d7831161ba1ef9de49c4
Petro <onisko417@gmail.com>
2024-10-14 13:35:31 +0300

    Deleted previous EmailSender and added new Azure function

--- EmailSender/Program.cs
+++ EmailSender/Program.cs
@@ -1,54 +1,27 @@
-using Azure.Extensions.AspNetCore.Configuration.Secrets;
-using Azure.Identity;
-using Azure.Messaging.ServiceBus;
-using EmailSender;
 using EmailSender.Models;
 using EmailSender.Services;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json;
 using RegApi.Shared.Models;
 
-var builder = Host.CreateApplicationBuilder(args);
+var host = new HostBuilder()
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services =>
+    {
+        services.AddApplicationInsightsTelemetryWorkerService();
+        services.ConfigureFunctionsApplicationInsights();
+        services.AddScoped<IEmailSenderService, EmailSenderService>();
 
-builder.Services.Configure<HostOptions>(options =>
-{
-    options.BackgroundServiceExceptionBehavior = BackgroundServiceExceptionBehavior.Ignore;
-});
+        var configuration = services.BuildServiceProvider().GetRequiredService<IConfiguration>();
 
-builder.Services.AddHostedService<Worker>();
+        var emailConfigurationJson = configuration["EmailConfiguration"];
+        var emailConfiguration = JsonConvert.DeserializeObject<EmailConfiguration>(emailConfigurationJson!);
 
-var keyVaultUri = new Uri(builder.Configuration["KeyVaultUri"]!);
-builder.Configuration.AddAzureKeyVault(keyVaultUri, new DefaultAzureCredential(), new AzureKeyVaultConfigurationOptions
-{
-    ReloadInterval = TimeSpan.FromMinutes(5)
-});
+        services.AddSingleton(emailConfiguration!);
+    })
+    .Build();
 
-var emailConfigJson = builder.Configuration["EmailConfiguration"];
-var emailConfig = JsonConvert.DeserializeObject<EmailConfiguration>(emailConfigJson!);
-
-var busServiceJson = builder.Configuration["BusService"];
-var busService = JsonConvert.DeserializeObject<BusServiceModel>(busServiceJson!);
-
-
-builder.Services.AddSingleton(emailConfig!);
-builder.Services.AddSingleton(busService!);
-
-builder.Services.AddScoped<IEmailSenderService, EmailSenderService>();
-
-
-builder.Services.AddScoped(provider =>
-{
-    var busService = provider.GetRequiredService<BusServiceModel>();
-    
-    return new ServiceBusClient(busService.ConnectionString);
-});
-
-builder.Services.AddScoped(provider =>
-{
-    var busService = provider.GetRequiredService<BusServiceModel>();
-    var client = new ServiceBusClient(busService.ConnectionString);
-
-    return client.CreateReceiver(busService.EmailQueue);
-});
-
-var host = builder.Build();
 host.Run();
---
+++ EmailSender/Startup.cs
@@ -0,0 +1,15 @@
+﻿using EmailSender.Services;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using NETCore.MailKit.Core;
+
+namespace EmailSender
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient();
+        }
+    }
+}
